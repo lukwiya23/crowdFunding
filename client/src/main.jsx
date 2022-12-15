3d8762038ae66929f8ae6ccdf6db94b1eb2a0732
lukwiya23 <luk23bonnie8@gmail.com>
2022-12-15 13:19:50 +0300

    client setup done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import "./styles/globals.css";
 
-import { StateContextProvider } from './context';
-import App from './App';
-import './index.css';
+// This is the chainId your dApp will work on.
+const activeChainId = ChainId.Mainnet;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Goerli}> 
-    <Router>
-      <StateContextProvider>
-        <App />
-      </StateContextProvider>
-    </Router>
-  </ThirdwebProvider> 
-)
+  <React.StrictMode>
+    <ThirdwebProvider desiredChainId={activeChainId}>
+      <App />
+    </ThirdwebProvider>
+  </React.StrictMode>
+);
